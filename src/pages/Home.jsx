d5a94c40061c643fb0d5024b09f79b2b66012862
okoro91 <mi.fracri91@gmail.com>
2025-07-15 09:02:00 +0100

    comment out

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import Hero from "../components/main/Hero";
 import Security from "../components/main/Security";
-// import Savings from "../components/main/Savings";
+
 import Invest from "../components/main/Invest";
 import Featured from "../components/main/Featured";
 import Customer from "../components/main/Customer";
@@ -11,7 +10,7 @@ const Home = () => {
     <div className="max-w-315 m-auto p-10">
       <Hero />
       <Security />
-      {/* <Savings /> */}
+
       <Invest />
       <Customer />
       <Featured />
