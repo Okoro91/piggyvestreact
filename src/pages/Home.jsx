39d3f680467e5a6dc39c68bf25890b8d00a46f5d
okoro91 <mi.fracri91@gmail.com>
2025-07-16 04:55:46 +0100

    added savings component to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Hero from "../components/main/Hero";
 import Security from "../components/main/Security";
-
+import Savings from "../components/main/savings";
 const Home = () => {
   return (
     <div className="max-w-315 m-auto p-10">
       <Hero />
       <Security />
+      <Savings />
     </div>
   );
 };
