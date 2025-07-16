1647f739fde9b0918d88c265d2f1ecdca09142ca
okoro91 <mi.fracri91@gmail.com>
2025-07-16 10:33:01 +0100

    added invest components

--- src/components/main/invest.jsx
+++ src/components/main/invest.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import invest from "../../assets/Invest.avif";
 
 const Invest = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/main/Hero";
 import Security from "../components/main/Security";
 import Savings from "../components/main/savings";
+import Invest from "../components/main/Invest";
 const Home = () => {
   return (
     <div className="max-w-315 m-auto p-10">
       <Hero />
       <Security />
       <Savings />
+      <Invest />
     </div>
   );
 };
